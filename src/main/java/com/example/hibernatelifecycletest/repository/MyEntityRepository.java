36582ba1ab98b8cca947537343d751623fbba82e
wlsgur073 <wlsgur073@gmail.com>
2024-10-17 14:32:50 +0900

    don't need it

--- src/main/java/com/example/hibernatelifecycletest/repository/MyEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hibernatelifecycletest.repository;
-
-import com.example.hibernatelifecycletest.entity.MyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyEntityRepository extends JpaRepository<MyEntity, Long> {
-}
